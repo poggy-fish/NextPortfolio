2e1371e4ca0726a8a1d2527d1925cafa3f7a8c97
thesiya-nikunj <wd.nikunj@gmail.com>
2021-06-15 09:58:24 +0530

    Tooltip Added

--- src/components/SkillCard.js
+++ src/components/SkillCard.js
@@ -1,3 +1,5 @@
+import ReactTooltip from "react-tooltip";
+
 function SkillCard({ img, title }) {
 	return (
 		<div className="h-32 sm:h-44 md:w-auto md:py-8 md:px-14 bg-white flex items-center justify-center md:h-48 xl:h-44 rounded-3xl border border-transparent hover:bg-nik-lightGray hover:border-gray-300 cursor-pointer hover:shadow-xl">
@@ -5,7 +7,9 @@ function SkillCard({ img, title }) {
 				src={img}
 				alt={title}
 				className="w-14 h-14 sm:w-20 sm:h-20 md:w-32 xl:h-20 xl:w-28 object-contain"
+				data-tip={title}
 			/>
+			<ReactTooltip />
 		</div>
 	);
 }
